b8d5121ead1657d879cb1247b8bc39b096e5abae
TejasviChude-19 <chudetejasvi@gmail.com>
2024-04-24 21:11:02 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdminDashboardDataComponent } from './admin-dashboard-data/admin-dashboard-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/profile', pathMatch: 'full' }, // Redirect to profile by default
+  { path: 'profile', component: ProfileComponent },
+  { path: 'admin/login', component: AdminLoginComponent },
+  { path: 'admin/dashboard/data', component: AdminDashboardDataComponent },
+  { path: '**', redirectTo: '/profile' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
